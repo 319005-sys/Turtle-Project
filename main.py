554a0d953635084f3298f44b491cc15d95fdfbf6
319005-sys <319005@stu.wvusd.org>
2025-11-18 09:08:45 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,87 @@
-#this is where we will save our code
 import turtle as trtl
+
+def check_win(turtles):
+  winning = [
+    (0, 1, 2),
+    (3, 4, 5),
+    (6, 7, 8),
+    (0, 3, 6),
+    (1, 4, 7),
+    (2, 5, 8),
+    (0, 4, 8),
+    (2, 4, 6)
+  ]
+
+  positions = []
+  for t in turtles:
+    if t.isvisible() == True:
+      col = t.rect.x // 80
+      row = t.rect.y // 80
+      index = row * 3 + col
+      positions.append(index)
+
+  for a, b, c in winning:
+    if a in positions and b in positions and c in positions:
+      return True
+
+  return False
+
 
 turtles = []
 
-'''
-turtle1 = trtl.Turtle()
-turtle2 = trtl.Turtle()
-turtle3 = trtl.Turtle()
-turtle4 = trtl.Turtle()
-turtle5 = trtl.Turtle()
-turtle6 = trtl.Turtle()
-turtle7 = trtl.Turtle()
-turtle8 = trtl.Turtle()
-turtle9 = trtl.Turtle()
-turtles.append(turtle1)
-turtles.append(turtle2)
-turtles.append(turtle3)
-turtles.append(turtle4)
-turtles.append(turtle5)
-turtles.append(turtle6)
-turtles.append(turtle7)
-turtles.append(turtle8)
-turtles.append(turtle9)
-'''
-
 for i in range(9):
   turtle = trtl.Turtle()
+  turtle.speed(0)
+  turtle.penup()
+  turtle.shape("circle")
+  turtle.turtlesize(3)
   turtle.hideturtle()
   turtles.append(turtle)
 
 drawer = trtl.Turtle()
+drawer.hideturtle()
+drawer.speed(0)
+drawer.penup()
+drawer.goto(-100,-40)
+drawer.pendown()
+drawer.forward(200)
+
+drawer.penup()
+drawer.goto(-100,40)
+drawer.pendown()
+drawer.forward(200)
+
+drawer.penup()
+drawer.goto(-40,-100)
+drawer.setheading(90)
+drawer.pendown()
+drawer.forward(200)
+
+drawer.penup()
+drawer.goto(40,-100)
+drawer.setheading(90)
+drawer.pendown()
+drawer.forward(200)
+
+ROWS = 3
+COLS = 3
+CELL_SIZE = 80
+OFFSET_X = -80
+OFFSET_Y = -80
+
+positions = []
+
+for i in range(len(turtles)):
+    row = i // COLS
+    col = i % COLS
+    x = OFFSET_X + col * CELL_SIZE
+    y = OFFSET_Y + row * CELL_SIZE
+    positions.append((x, y))
+
+x=0
+for turtle in turtles:
+  turtle.goto(positions[x])
+  x += 1
   
 wn = trtl.Screen()
 wn.mainloop()
